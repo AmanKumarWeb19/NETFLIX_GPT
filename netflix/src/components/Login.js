9b4e8528313490e7b4983cfd03f5ea66f2e003bd
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-05-06 12:57:06 +0530

    login page

--- netflix/src/components/Login.js
+++ netflix/src/components/Login.js
@@ -5,12 +5,29 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <div>
+      <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/d253acf4-a1e2-4462-a416-f78802dc2d85/f04bf88c-f71c-4d02-82ed-adb870b8f8db/IN-en-20240429-POP_SIGNUP_TWO_WEEKS-perspective_WEB_658a042e-62cf-473d-8da0-7b875f23e2ef_large.jpg"
-          alt="background-image"
+          alt="bg-img"
         />
       </div>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Enter Email"
+          className="p-4 my-4 w-full bg-gray-800 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          className="p-4 my-4 w-full bg-gray-800 rounded-lg"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          Sign In
+        </button>
+        <p className="py-4">New to Netflix? Sign Up Now!</p>
+      </form>
     </div>
   );
 };
