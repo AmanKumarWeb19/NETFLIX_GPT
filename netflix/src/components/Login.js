81d1149ca9665ad4f402dca9dbcaa5e825fef108
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-05-06 12:25:55 +0530

    add background image and netflix logo

--- netflix/src/components/Login.js
+++ netflix/src/components/Login.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/d253acf4-a1e2-4462-a416-f78802dc2d85/f04bf88c-f71c-4d02-82ed-adb870b8f8db/IN-en-20240429-POP_SIGNUP_TWO_WEEKS-perspective_WEB_658a042e-62cf-473d-8da0-7b875f23e2ef_large.jpg"
+          alt="background-image"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
